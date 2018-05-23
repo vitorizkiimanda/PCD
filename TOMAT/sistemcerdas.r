0b32a0601716ce55701d425b394db02214b57ec7
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-05-23 08:05:17 +0700

    improved

--- TOMAT/sistemcerdas.r
+++ TOMAT/sistemcerdas.r
@@ -2,7 +2,7 @@
 ###decision tree : dibawah + randomForest + randomForest
 #membagi data
 library(readxl)
-tomat <- read_excel("E:/VRI/IPB/kuliah/Sem6/PCD/PCD/TOMAT/tomatKematangan.xlsx")
+tomat <- read_excel("E:/VRI/IPB/kuliah/Sem6/PCD/PCD/TOMAT/tomatRGBHSV.xlsx")
 tomat$kematangan <- as.factor(tomat$kematangan)
 set.seed(1234)#ini buat ngerandom
 ind <- sample(2, nrow(tomat), replace=TRUE, prob=c(0.7,0.3))#ini buat misahin data ke n data -> 2 kalo disini : 70% 30%
@@ -12,14 +12,14 @@ testData <- tomat[ind==2,] # ini buat 0.3 -> 30%
 #partykit Library -> menggunakan nilai statistik 
 library(partykit)
 modelctree <- ctree(kematangan ~ .,data = trainData) #Species = class ,,  . = semua atribut kita pake
-predctree <- predict(modelctree, testData[,-4]) # -5 = apus atribut ke-5
+predctree <- predict(modelctree, testData[,-7]) # -5 = apus atribut ke-5
 table(predctree, testData$kematangan)
 plot(predctree, testData$kematangan)
 
 #rpart library -> menggunakan nilai bias
 library(rpart)
 modelrpart <- rpart(kematangan ~ .,data = trainData)
-predrpart <- predict(modelrpart, type="class", testData[,-4])
+predrpart <- predict(modelrpart, type="class", testData[,-7])
 table(predrpart, testData$kematangan)
 plot(predrpart)
 
@@ -27,14 +27,16 @@ plot(predrpart)
 #e1071 library
 library(e1071)
 svm.model <- svm(kematangan ~ .,data = trainData, cost = 100, gamma =1)
-svm.pred <- predict(svm.model, testData[,-4])
-table(pred = svm.pred, true = testData[,4])
+svm.pred <- predict(svm.model, testData[,-7])
+table(pred = svm.pred, true = testData[,7])
 plot(svm.pred, testData$kematangan)
 
 #tambahan
 #confusion matrix= library caret
 library(caret)
 confusionMatrix(svm.pred, testData$kematangan)
+confusionMatrix(predctree, testData$kematangan)
+confusionMatrix(predrpart, testData$kematangan)
 
 ###note
 # ?"namafungsi"   -> untuk summon help
--- tomat_imgToData.py
+++ tomat_imgToData.py
@@ -22,6 +22,14 @@ for imgname in imgnames:
     biruTotal = 0
     hijauTotal = 0
 
+    hue = 0
+    saturation = 0
+    value = 0
+
+    hueTotal = 0
+    saturationTotal = 0
+    valueTotal = 0
+
 
     #menghitung proporsional warna
     ## merah , orange, hijau
@@ -47,8 +55,30 @@ for imgname in imgnames:
     B = B / biru
     H = H/ hijau
 
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    for i in range(0, row):
+        for j in range(0, col):
+            h, s, v = img[i, j]
+            print(h,s,v)
+            hueTotal = hueTotal + h
+            saturationTotal = saturationTotal + s
+            valueTotal = valueTotal + v
+
+            if(h): hue = hue + 1
+            if(s): saturation = saturation + 1
+            if(v): value = value + 1
+
+    H2 = hueTotal
+    S = saturationTotal
+    V = valueTotal
+
+    H2 = H2 / hue
+    S = S / saturation
+    V = V / value
+
     # data = imgname + "," + merah + "," + hijau + "\n"
-    data = '%s,%f,%f,%f' % (imgname, M,H,B)
+    data = '%s,%f,%f,%f,%f,%f,%f' % (imgname, M,H,B,H2,S,V)
     data = data + "\n"
     print(data)
     csv.write(data)
