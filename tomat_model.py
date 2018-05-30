71ee2220eb8067a8fb32a1e45ac2bd6e53f2a750
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-05-30 21:46:57 +0700

    model creation

--- tomat_model.py
+++ tomat_model.py
@@ -1,4 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+
 import pandas as pd
 import numpy as np
 
@@ -6,44 +8,66 @@ import pickle
 
 from sklearn.metrics import confusion_matrix
 
-data = pd.read_csv('tomatKematangan.csv')
-#print(data)
+#import dataset
+dataKematangan = pd.read_csv('tomatKematangan.csv')
+dataBerat = pd.read_csv('tomatBerat.csv')
 
-train=data.sample(frac=0.8,random_state=200)
-test= data.sample(frac=0.2,random_state=200)
+#split data test dan data train
+trainKematangan=dataKematangan.sample(frac=0.8,random_state=200)
+testKematangan= dataKematangan.sample(frac=0.2,random_state=200)
 
-kematanganTrain = train["kematangan"]
-#print(kematangan)
+trainBerat=dataBerat.sample(frac=0.8,random_state=200)
+testBerat= dataBerat.sample(frac=0.2,random_state=200)
 
-train = train.drop(["kematangan"], axis=1)
-#print(train)
+#mengambil class dari dataset
+kematanganTrain = trainKematangan["kematangan"]
+beratTrain = trainBerat["berat"]
 
-model = RandomForestClassifier(max_depth=2, random_state=0)
-model.fit(train,kematanganTrain)
-
-
-######test
-kematanganTest = test["kematangan"]
-test=test.drop(["kematangan"], axis=1)
+#memisahkan class dari dataset
+trainKematangan = trainKematangan.drop(["kematangan"], axis=1)
+trainBerat = trainBerat.drop(["berat"], axis=1)
 
 
-####predict
-kematanganPredict = model.predict(test)  #nnti ini test : inputan baru
-#print(kematanganPredict)
+#membuat model menggunakan random forest
+modelKematangan = RandomForestClassifier(max_depth=10, random_state=0)
+modelKematangan.fit(trainKematangan,kematanganTrain)
 
+modelBerat = RandomForestRegressor(max_depth=20, random_state=0)
+modelBerat.fit(trainBerat,beratTrain)
+
+
+###### data test
+kematanganTestAsli = testKematangan["kematangan"]
+testKematangan=testKematangan.drop(["kematangan"], axis=1)
+
+beratTestAsli = testBerat["berat"]
+testBerat=testBerat.drop(["berat"], axis=1)
+
+
+####prediksi data test menggunakan model random forest
+kematanganPredict = modelKematangan.predict(testKematangan)  #nnti ini test : inputan baru
+beratPredict = modelBerat.predict(testBerat)
+print(beratPredict)
+print(beratTestAsli)
 
 ####confusion matrix
-print(confusion_matrix(kematanganTest, kematanganPredict))
+print(confusion_matrix(kematanganTestAsli, kematanganPredict))
 
 
 
 
 # save the model to disk
-filename = 'modelTomat.sav'
-pickle.dump(model, open(filename, 'wb'))
+filename = 'modelTomatKematangan.sav'
+pickle.dump(modelKematangan, open(filename, 'wb'))
+
+filename = 'modelTomatBerat.sav'
+pickle.dump(modelKematangan, open(filename, 'wb'))
+
+
+
 
 
 # load the model from disk
 loaded_model = pickle.load(open(filename, 'rb'))
-kematanganPredict = loaded_model.predict(test)  #nnti ini test : inputan baru
-print(kematanganPredict)
+kematanganPredict = loaded_model.predict(testKematangan)  #nnti ini test : inputan baru
+#print(kematanganPredict)
